5ec5a7be412ed31a67385bcdec6da48b95b43ed4
Wargas Teixeira <teixeira.wargas@gmail.com>
2020-02-14 20:55:15 -0300

    preparando para deploy

--- start/routes.js
+++ start/routes.js
@@ -4,15 +4,17 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
+Route.get('/teste', ({ view }) => {
+  return view.render('index')
 })
 
-Route.post('auth', 'AuthController.create')
-Route.post('auth/login', 'AuthController.login')
-Route.get('auth', 'AuthController.index')
-Route.get('me', 'AuthController.currentUser').middleware('auth')
+Route.group(() => {
+  Route.post('auth', 'AuthController.create')
+  Route.post('auth/login', 'AuthController.login')
+  Route.get('auth', 'AuthController.index')
+  Route.get('me', 'AuthController.currentUser').middleware('auth')
 
-Route.resource('disciplinas', 'DisciplinaController').middleware('auth')
-Route.resource('disciplinas/:disciplina_id/aulas', 'AulaController').middleware('auth')
-Route.resource('controle', 'ControleController')
+  Route.resource('disciplinas', 'DisciplinaController').middleware('auth')
+  Route.resource('disciplinas/:disciplina_id/aulas', 'AulaController').middleware('auth')
+  Route.resource('controle', 'ControleController')
+}).prefix('api/v4')
